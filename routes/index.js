206f59afc9fbaed30f7dfac0de77573150aa8afd
Josh Bivens <hotbioform@gmail.com>
2015-11-29 15:31:56 -0700

    Add single post and post-by-id methods

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,11 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
+// Single post
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
 router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
 
@@ -25,6 +30,18 @@ router.post('/posts', function(req, res, next) {
     if(err){ return next(err); }
 
     res.json(post);
+  });
+});
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
   });
 });
 
