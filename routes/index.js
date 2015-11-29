48092c6dc5cd2a3097c45d8b642b30f21cbaada5
Josh Bivens <hotbioform@gmail.com>
2015-11-29 16:20:45 -0700

    Add upvote and comment routes and params

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
-/* GET home page. */
+// GET
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -18,11 +18,30 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-// Single post
 router.get('/posts/:post', function(req, res) {
   res.json(req.post);
 });
 
+
+// PUT
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if(err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, post) { // <<< FIX
+    if(err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
+
+// POST
 router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
 
@@ -33,6 +52,24 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+router.post('/posts/:post/comments', function(req,res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment) {
+    if(err) { return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err) { return next(err); }
+
+      req.json(comment);
+    });
+  });
+});
+
+
+// MIDDLEWARE
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
 
@@ -41,6 +78,18 @@ router.param('post', function(req, res, next, id) {
     if (!post) { return next(new Error('can\'t find post')); }
 
     req.post = post;
+    return next();
+  });
+});
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id); // <<< START HERE
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
     return next();
   });
 });
