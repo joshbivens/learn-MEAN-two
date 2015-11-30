03a8488e56cf134cf7c51bd498026b682ff3ae79
Josh Bivens <hotbioform@gmail.com>
2015-11-30 15:30:47 -0700

    Add 'upvote' method

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -29,16 +29,25 @@ angular.module('flapperNews', ['ui.router'])
     posts: []
   };
   o.getAll = function() {
-    return $http.get('/posts').success(function(data) {
-      angular.copy(data, o.posts);
-    });
-  }
+    return $http.get('/posts')
+      .success(function(data) {
+        angular.copy(data, o.posts);
+      });
+  };
 
   o.create = function(post) {
-    return $http.post('/posts', post).success(function(data) {
-      o.posts.push(data);
-    })
-  }
+    return $http.post('/posts', post)
+      .success(function(data) {
+        o.posts.push(data);
+      });
+  };
+
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+      .success(function(data) {
+        post.upvotes += 1
+      });
+  };
 
   return o;
 }])
@@ -60,7 +69,7 @@ angular.module('flapperNews', ['ui.router'])
     };
 
     $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+      posts.upvote(post);
     }
   }
 ])
