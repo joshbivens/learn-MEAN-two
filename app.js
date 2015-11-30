f34daf6fccb30b379fdc4b04de345e2e6c7768d6
Josh Bivens <hotbioform@gmail.com>
2015-11-30 14:54:22 -0700

    Reorganize

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/news');
 require('./models/Posts');
 require('./models/Comments');
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require('mongoose');
-var Post = mongoose.model('Post');
-var Comment = mongoose.model('Comment');
-
-// GET
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts) {
@@ -18,34 +17,6 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.get('/posts/:post', function(req, res, next) {
-  res.post.populate('comments', function(err, post) {
-    if(err) { return next(err); }
-
-    res.json(post);
-  });
-});
-
-
-// PUT
-router.put('/posts/:post/upvote', function(req, res, next) {
-  req.post.upvote(function(err, post) {
-    if(err) { return next(err); }
-
-    res.json(post);
-  });
-});
-
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
-  req.comment.upvote(function(err, post) {
-    if(err) { return next(err); }
-
-    res.json(comment);
-  });
-});
-
-
-// POST
 router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
 
@@ -56,24 +27,6 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req,res, next) {
-  var comment = new Comment(req.body);
-  comment.post = req.post;
-
-  comment.save(function(err, comment) {
-    if(err) { return next(err); }
-
-    req.post.comments.push(comment);
-    req.post.save(function(err, post) {
-      if(err) { return next(err); }
-
-      req.json(comment);
-    });
-  });
-});
-
-
-// MIDDLEWARE
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
 
@@ -87,14 +40,54 @@ router.param('post', function(req, res, next, id) {
 });
 
 router.param('comment', function(req, res, next, id) {
-  var query = Comment.findById(id); // <<< START HERE
+  var query = Comment.findById(id);
 
-  query.exec(function (err, comment){
+  query.exec(function (err, post){
     if (err) { return next(err); }
     if (!comment) { return next(new Error('can\'t find comment')); }
 
     req.comment = comment;
     return next();
+  });
+});
+
+router.get('/posts/:post', function(req,res) {
+  res.post.populate('comments', function(err, post) {
+    if(err) { return next(err); }
+    
+    res.json(post);
+  });
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if(err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if(err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment) {
+    if(err) { return next(err); }
+
+    req.post.comment.push(comment);
+    req.post.save(function() {
+      if(err) { return next(err); }
+
+      res.json(comment);
+    });
   });
 });
 
