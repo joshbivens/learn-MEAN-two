801ed3489d247149ac6e9222a0b77c542a67964b
Josh Bivens <hotbioform@gmail.com>
2015-12-01 15:41:55 -0700

    Add exprress-jwt and require auth for posting, commenting, upvoting

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var jwt = require('express-jwt');
 var router = express.Router();
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -53,7 +55,7 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
 
   post.save(function(err, post) {
@@ -95,7 +97,7 @@ router.get('/posts/:post', function(req,res) {
   });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post) {
     if(err) { return next(err); }
 
@@ -103,7 +105,7 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment) {
     if(err) { return next(err); }
 
@@ -111,7 +113,7 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
 
