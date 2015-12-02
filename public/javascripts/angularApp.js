7e7a269f6cd499c0a510d13b19d7f56914b02fb8
Josh Bivens <hotbioform@gmail.com>
2015-12-02 15:32:20 -0700

    Fix typos and reorganize

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -30,7 +30,7 @@ angular.module('flapperNews', ['ui.router'])
         templateUrl: '/login.html',
         controller: 'AuthCtrl',
         onEnter: ['$state', 'auth', function($state, auth) {
-          if(auth.isLoggedIn()){
+          if(auth.isLoggedIn()) {
             $state.go('home');
           }
         }]
@@ -65,14 +65,14 @@ angular.module('flapperNews', ['ui.router'])
 
     if(token) {
       var payload = JSON.parse($window.atob(token.split('.')[1]));
-      return payload.exp = Date.now() / 1000;
+      return payload.exp > Date.now() / 1000;
     } else {
       return false;
     }
   }
 
   auth.currentUser = function() {
-    if(auth.isLoggedIn) {
+    if(auth.isLoggedIn()) {
       var token = auth.getToken();
       var payload = JSON.parse($window.atob(token.split('.')[1]));
 
@@ -100,39 +100,6 @@ angular.module('flapperNews', ['ui.router'])
 
   return auth;
 }])
-.controller('AuthCtrl', [
-  '$scope',
-  '$state',
-  'auth',
-  function($scope, $state, auth) {
-    $scope.user = {};
-
-    $scope.register = function() {
-      auth.register($scope.user).error(function() {
-        $scope.error = error;
-      }).then(function() {
-        $state.go('home');
-      });
-    };
-
-    $scope.logIn = function() {
-      auth.logIn($scope.user).error(function() {
-        $scope.error = error;
-      }).then(function() {
-        $state.go('home');
-      });
-    };
-  };
-])
-.controller('NavCtrl' [
-  '$scope',
-  'auth',
-  function ($scope, auth) {
-    $scope.isLoggedIn = auth.isLoggedIn;
-    $scope.currentUser = auth.currentUser;
-    $scope.logOut = auth.logOut;
-  }
-])
 .factory('posts', ['$http', function($http){
   var o = {
     posts: []
@@ -221,4 +188,37 @@ angular.module('flapperNews', ['ui.router'])
       posts.upvoteComment(post, comment);
     }
   }
+])
+.controller('AuthCtrl', [
+  '$scope',
+  '$state',
+  'auth',
+  function($scope, $state, auth) {
+    $scope.user = {};
+
+    $scope.register = function() {
+      auth.register($scope.user).error(function(error) {
+        $scope.error = error;
+      }).then(function() {
+        $state.go('home');
+      });
+    };
+
+    $scope.logIn = function() {
+      auth.logIn($scope.user).error(function(error) {
+        $scope.error = error;
+      }).then(function() {
+        $state.go('home');
+      });
+    };
+  }
+])
+.controller('NavCtrl', [
+  '$scope',
+  'auth',
+  function($scope, auth){
+    $scope.isLoggedIn = auth.isLoggedIn;
+    $scope.currentUser = auth.currentUser;
+    $scope.logOut = auth.logOut;
+  } 
 ]);
